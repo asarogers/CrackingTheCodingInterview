56a48e1072703bd3c2574cd79e0a3bfce077d769
Ace <cyberasasoftware@gmail.com>
2023-02-18 13:42:00 -0600

    inital

--- tests/leetcode.js
+++ tests/leetcode.js
@@ -1,29 +1,27 @@
-dataset = [4, 3, 5, 7, 9, 10, 8]
+// Function to sort an array using insertion sort
+function insertionSort(arr, n) 
+{ 
+    let i, key, j; 
+    for (i = 1; i < n; i++)
+    { 
+        key = arr[i]; 
+        j = i - 1; 
+   
+        /* Move elements of arr[0..i-1], that are 
+        greater than key, to one position ahead 
+        of their current position */
+        console.log(arr)
+        while (j >= 0 && arr[j] > key)
+        { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+        
+    } 
 
-quickSort(dataset, 6)
-function quickSort(array, index){
-    dataReturn = [];
-    var middle = index/2;
-    //check if middle is greater/lesser than pivot
-    dataReturn[middle] = array[index]
-    if(array[middle] > dataReturn[middle]){
-        dataReturn[middle+1] = array[middle]
-    } else{
-        dataReturn[middle-1] = array[middle]
-    }
+} 
 
-    //check if all values less than index are greater/lesser than pivot
-    for (let i = 0; i < array.length; i++) {
-        //if greater than, put them above half way point
-        if(array[i] > dataReturn[middle]){
-            dataReturn.push(array[i]);
-        }
-        else{
-            //if lesser than, put them below half way point
-            dataReturn.unshift(array[i]);
-        }
-    }
-
-    console.log(dataReturn)
-    
-}
+arr = [39, 20, 1, 2, 17, 5, 3];
+console.log(arr.length)
+insertionSort(arr,arr.length)
